ea9de215acd0589ff197df138f5eb4449f9cf412
andreachou <andrea.y.chou@gmail.com>
2022-05-18 13:58:09 -0700

    revise

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function like(id) {
     document.getElementById(id).innerHTML++;
 }
 
-/* works the same as above
+/* works the same as above for just specific one button
 function like() {
     document.querySelector('[id="li"]').innerHTML++;
 }
